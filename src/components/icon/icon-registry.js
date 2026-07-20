aa032bd59d3f8b7a549c7343bc59d01f15b8d5fc
Matthew Pichola <41563042+mattpichola@users.noreply.github.com>
2026-07-20 14:05:46 -0400

    icons added and configured

--- src/components/icon/icon-registry.js
+++ src/components/icon/icon-registry.js
@@ -1,234 +1,234 @@
 export const ICONS = {
     bell: {
-        base: './components/icon/icons/base/bell.svg',
-        sm: './components/icon/icons/small/bell.svg'
+        base: '/src/components/icon/icons/base/bell.svg',
+        sm: '/src/components/icon/icons/small/bell.svg'
     },
     book: {
-        base: './components/icon/icons/base/book.svg',
-        sm: './components/icon/icons/small/book.svg'
+        base: '/src/components/icon/icons/base/book.svg',
+        sm: '/src/components/icon/icons/small/book.svg'
     },
     bookmark: {
-        base: './components/icon/icons/base/bookmark.svg',
-        sm: './components/icon/icons/small/bookmark.svg'
+        base: '/src/components/icon/icons/base/bookmark.svg',
+        sm: '/src/components/icon/icons/small/bookmark.svg'
     },
     box: {
-        base: './components/icon/icons/base/box.svg',
-        sm: './components/icon/icons/small/box.svg'
+        base: '/src/components/icon/icons/base/box.svg',
+        sm: '/src/components/icon/icons/small/box.svg'
     },
     browser: {
-        base: './components/icon/icons/base/browser.svg',
-        sm: './components/icon/icons/small/browser.svg'
+        base: '/src/components/icon/icons/base/browser.svg',
+        sm: '/src/components/icon/icons/small/browser.svg'
     },
     bubble: {
-        base: './components/icon/icons/base/bubble.svg',
-        sm: './components/icon/icons/small/bubble.svg'
+        base: '/src/components/icon/icons/base/bubble.svg',
+        sm: '/src/components/icon/icons/small/bubble.svg'
     },
     bubbles: {
-        base: './components/icon/icons/base/bubbles.svg',
-        sm: './components/icon/icons/small/bubbles.svg'
+        base: '/src/components/icon/icons/base/bubbles.svg',
+        sm: '/src/components/icon/icons/small/bubbles.svg'
     },
     calendar: {
-        base: './components/icon/icons/base/calendar.svg',
-        sm: './components/icon/icons/small/calendar.svg'
+        base: '/src/components/icon/icons/base/calendar.svg',
+        sm: '/src/components/icon/icons/small/calendar.svg'
     },
     camera: {
-        base: './components/icon/icons/base/camera.svg',
-        sm: './components/icon/icons/small/camera.svg'
+        base: '/src/components/icon/icons/base/camera.svg',
+        sm: '/src/components/icon/icons/small/camera.svg'
     },
     cancel: {
-        base: './components/icon/icons/base/cancel.svg',
-        sm: './components/icon/icons/small/cancel.svg'
+        base: '/src/components/icon/icons/base/cancel.svg',
+        sm: '/src/components/icon/icons/small/cancel.svg'
     },
     cart: {
-        base: './components/icon/icons/base/cart.svg',
-        sm: './components/icon/icons/small/cart.svg'
+        base: '/src/components/icon/icons/base/cart.svg',
+        sm: '/src/components/icon/icons/small/cart.svg'
     },
     chart: {
-        base: './components/icon/icons/base/chart.svg',
-        sm: './components/icon/icons/small/chart.svg'
+        base: '/src/components/icon/icons/base/chart.svg',
+        sm: '/src/components/icon/icons/small/chart.svg'
     },
     clock: {
-        base: './components/icon/icons/base/clock.svg',
-        sm: './components/icon/icons/small/clock.svg'
+        base: '/src/components/icon/icons/base/clock.svg',
+        sm: '/src/components/icon/icons/small/clock.svg'
     },
     cloud: {
-        base: './components/icon/icons/base/cloud.svg',
-        sm: './components/icon/icons/small/cloud.svg'
+        base: '/src/components/icon/icons/base/cloud.svg',
+        sm: '/src/components/icon/icons/small/cloud.svg'
     },
     cog: {
-        base: './components/icon/icons/base/cog.svg',
-        sm: './components/icon/icons/small/cog.svg'
+        base: '/src/components/icon/icons/base/cog.svg',
+        sm: '/src/components/icon/icons/small/cog.svg'
     },
     cross: {
-        base: './components/icon/icons/base/cross.svg',
-        sm: './components/icon/icons/small/cross.svg'
+        base: '/src/components/icon/icons/base/cross.svg',
+        sm: '/src/components/icon/icons/small/cross.svg'
     },
     dashboard: {
-        base: './components/icon/icons/base/dashboard.svg',
-        sm: './components/icon/icons/small/dashboard.svg'
+        base: '/src/components/icon/icons/base/dashboard.svg',
+        sm: '/src/components/icon/icons/small/dashboard.svg'
     },
     download: {
-        base: './components/icon/icons/base/download.svg',
-        sm: './components/icon/icons/small/download.svg'
+        base: '/src/components/icon/icons/base/download.svg',
+        sm: '/src/components/icon/icons/small/download.svg'
     },
     envelope: {
-        base: './components/icon/icons/base/envelope.svg',
-        sm: './components/icon/icons/small/envelope.svg'
+        base: '/src/components/icon/icons/base/envelope.svg',
+        sm: '/src/components/icon/icons/small/envelope.svg'
     },
     eye: {
-        base: './components/icon/icons/base/eye.svg',
-        sm: './components/icon/icons/small/eye.svg'
+        base: '/src/components/icon/icons/base/eye.svg',
+        sm: '/src/components/icon/icons/small/eye.svg'
     },
     fast_forward: {
-        base: './components/icon/icons/base/fast-forward.svg',
-        sm: './components/icon/icons/small/fast-forward.svg'
+        base: '/src/components/icon/icons/base/fast-forward.svg',
+        sm: '/src/components/icon/icons/small/fast-forward.svg'
     },
     film: {
-        base: './components/icon/icons/base/film.svg',
-        sm: './components/icon/icons/small/film.svg'
+        base: '/src/components/icon/icons/base/film.svg',
+        sm: '/src/components/icon/icons/small/film.svg'
     },
     flag: {
-        base: './components/icon/icons/base/flag.svg',
-        sm: './components/icon/icons/small/flag.svg'
+        base: '/src/components/icon/icons/base/flag.svg',
+        sm: '/src/components/icon/icons/small/flag.svg'
     },
     folder: {
-        base: './components/icon/icons/base/folder.svg',
-        sm: './components/icon/icons/small/folder.svg'
+        base: '/src/components/icon/icons/base/folder.svg',
+        sm: '/src/components/icon/icons/small/folder.svg'
     },
     forward: {
-        base: './components/icon/icons/base/forward.svg',
-        sm: './components/icon/icons/small/forward.svg'
+        base: '/src/components/icon/icons/base/forward.svg',
+        sm: '/src/components/icon/icons/small/forward.svg'
     },
     heart: {
-        base: './components/icon/icons/base/heart.svg',
-        sm: './components/icon/icons/small/heart.svg'
+        base: '/src/components/icon/icons/base/heart.svg',
+        sm: '/src/components/icon/icons/small/heart.svg'
     },
     home: {
-        base: './components/icon/icons/base/home.svg',
-        sm: './components/icon/icons/small/home.svg'
+        base: '/src/components/icon/icons/base/home.svg',
+        sm: '/src/components/icon/icons/small/home.svg'
     },
     image: {
-        base: './components/icon/icons/base/image.svg',
-        sm: './components/icon/icons/small/image.svg'
+        base: '/src/components/icon/icons/base/image.svg',
+        sm: '/src/components/icon/icons/small/image.svg'
     },
     left: {
-        base: './components/icon/icons/base/left.svg',
-        sm: './components/icon/icons/small/left.svg'
+        base: '/src/components/icon/icons/base/left.svg',
+        sm: '/src/components/icon/icons/small/left.svg'
     },
     location: {
-        base: './components/icon/icons/base/location.svg',
-        sm: './components/icon/icons/small/location.svg'
+        base: '/src/components/icon/icons/base/location.svg',
+        sm: '/src/components/icon/icons/small/location.svg'
     },
     map_marker: {
-        base: './components/icon/icons/base/map-marker.svg',
-        sm: './components/icon/icons/small/map-marker.svg'
+        base: '/src/components/icon/icons/base/map-marker.svg',
+        sm: '/src/components/icon/icons/small/map-marker.svg'
     },
     microphone: {
-        base: './components/icon/icons/base/microphone.svg',
-        sm: './components/icon/icons/small/microphone.svg'
+        base: '/src/components/icon/icons/base/microphone.svg',
+        sm: '/src/components/icon/icons/small/microphone.svg'
     },
     moon: {
-        base: './components/icon/icons/base/moon.svg',
-        sm: './components/icon/icons/small/moon.svg'
+        base: '/src/components/icon/icons/base/moon.svg',
+        sm: '/src/components/icon/icons/small/moon.svg'
     },
     next: {
-        base: './components/icon/icons/base/next.svg',
-        sm: './components/icon/icons/small/next.svg'
+        base: '/src/components/icon/icons/base/next.svg',
+        sm: '/src/components/icon/icons/small/next.svg'
     },
     play: {
-        base: './components/icon/icons/base/play.svg',
-        sm: './components/icon/icons/small/play.svg'
+        base: '/src/components/icon/icons/base/play.svg',
+        sm: '/src/components/icon/icons/small/play.svg'
     },
     plus: {
-        base: './components/icon/icons/base/plus.svg',
-        sm: './components/icon/icons/small/plus.svg'
+        base: '/src/components/icon/icons/base/plus.svg',
+        sm: '/src/components/icon/icons/small/plus.svg'
     },
     printer: {
-        base: './components/icon/icons/base/printer.svg',
-        sm: './components/icon/icons/small/printer.svg'
+        base: '/src/components/icon/icons/base/printer.svg',
+        sm: '/src/components/icon/icons/small/printer.svg'
     },
     rain: {
-        base: './components/icon/icons/base/rain.svg',
-        sm: './components/icon/icons/small/rain.svg'
+        base: '/src/components/icon/icons/base/rain.svg',
+        sm: '/src/components/icon/icons/small/rain.svg'
     },
     refresh: {
-        base: './components/icon/icons/base/refresh.svg',
-        sm: './components/icon/icons/small/refresh.svg'
+        base: '/src/components/icon/icons/base/refresh.svg',
+        sm: '/src/components/icon/icons/small/refresh.svg'
     },
     reply_all: {
-        base: './components/icon/icons/base/reply-all.svg',
-        sm: './components/icon/icons/small/reply-all.svg'
+        base: '/src/components/icon/icons/base/reply-all.svg',
+        sm: '/src/components/icon/icons/small/reply-all.svg'
     },
     reply: {
-        base: './components/icon/icons/base/reply.svg',
-        sm: './components/icon/icons/small/reply.svg'
+        base: '/src/components/icon/icons/base/reply.svg',
+        sm: '/src/components/icon/icons/small/reply.svg'
     },
     search: {
-        base: './components/icon/icons/base/search.svg',
-        sm: './components/icon/icons/small/search.svg'
+        base: '/src/components/icon/icons/base/search.svg',
+        sm: '/src/components/icon/icons/small/search.svg'
     },
     sound: {
-        base: './components/icon/icons/base/sound.svg',
-        sm: './components/icon/icons/small/sound.svg'
+        base: '/src/components/icon/icons/base/sound.svg',
+        sm: '/src/components/icon/icons/small/sound.svg'
     },
     store: {
-        base: './components/icon/icons/base/store.svg',
-        sm: './components/icon/icons/small/store.svg'
+        base: '/src/components/icon/icons/base/store.svg',
+        sm: '/src/components/icon/icons/small/store.svg'
     },
     sun: {
-        base: './components/icon/icons/base/sun.svg',
-        sm: './components/icon/icons/small/sun.svg'
+        base: '/src/components/icon/icons/base/sun.svg',
+        sm: '/src/components/icon/icons/small/sun.svg'
     },
     support: {
-        base: './components/icon/icons/base/support.svg',
-        sm: './components/icon/icons/small/support.svg'
+        base: '/src/components/icon/icons/base/support.svg',
+        sm: '/src/components/icon/icons/small/support.svg'
     },
     switch: {
-        base: './components/icon/icons/base/switch.svg',
-        sm: './components/icon/icons/small/switch.svg'
+        base: '/src/components/icon/icons/base/switch.svg',
+        sm: '/src/components/icon/icons/small/switch.svg'
     },
     tag: {
-        base: './components/icon/icons/base/tag.svg',
-        sm: './components/icon/icons/small/tag.svg'
+        base: '/src/components/icon/icons/base/tag.svg',
+        sm: '/src/components/icon/icons/small/tag.svg'
     },
     target: {
-        base: './components/icon/icons/base/target.svg',
-        sm: './components/icon/icons/small/target.svg'
+        base: '/src/components/icon/icons/base/target.svg',
+        sm: '/src/components/icon/icons/small/target.svg'
     },
     trash: {
-        base: './components/icon/icons/base/trash.svg',
-        sm: './components/icon/icons/small/trash.svg'
+        base: '/src/components/icon/icons/base/trash.svg',
+        sm: '/src/components/icon/icons/small/trash.svg'
     },
     trophy: {
-        base: './components/icon/icons/base/trophy.svg',
-        sm: './components/icon/icons/small/trophy.svg'
+        base: '/src/components/icon/icons/base/trophy.svg',
+        sm: '/src/components/icon/icons/small/trophy.svg'
     },
     up: {
-        base: './components/icon/icons/base/up.svg',
-        sm: './components/icon/icons/small/up.svg'
+        base: '/src/components/icon/icons/base/up.svg',
+        sm: '/src/components/icon/icons/small/up.svg'
     },
     upload: {
-        base: './components/icon/icons/base/upload.svg',
-        sm: './components/icon/icons/small/upload.svg'
+        base: '/src/components/icon/icons/base/upload.svg',
+        sm: '/src/components/icon/icons/small/upload.svg'
     },
     user: {
-        base: './components/icon/icons/base/user.svg',
-        sm: './components/icon/icons/small/user.svg'
+        base: '/src/components/icon/icons/base/user.svg',
+        sm: '/src/components/icon/icons/small/user.svg'
     },
     users: {
-        base: './components/icon/icons/base/users.svg',
-        sm: './components/icon/icons/small/users.svg'
+        base: '/src/components/icon/icons/base/users.svg',
+        sm: '/src/components/icon/icons/small/users.svg'
     },
     video_camera: {
-        base: './components/icon/icons/base/video-camera.svg',
-        sm: './components/icon/icons/small/video-camera.svg'
+        base: '/src/components/icon/icons/base/video-camera.svg',
+        sm: '/src/components/icon/icons/small/video-camera.svg'
     },
     windows: {
-        base: './components/icon/icons/base/windows.svg',
-        sm: './components/icon/icons/small/windows.svg'
+        base: '/src/components/icon/icons/base/windows.svg',
+        sm: '/src/components/icon/icons/small/windows.svg'
     },
     write: {
-        base: './components/icon/icons/base/write.svg',
-        sm: './components/icon/icons/small/write.svg'
+        base: '/src/components/icon/icons/base/write.svg',
+        sm: '/src/components/icon/icons/small/write.svg'
     },
 }
